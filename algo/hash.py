db635ed57d544b850cff111c7ac382102afeb746
Donald Gonzales <79231585+dgonzales86@users.noreply.github.com>
2023-07-11 20:50:49 -0500

    adjusted hash table to store package objects added csv files for addresses and distance table

--- algo/hash.py
+++ algo/hash.py
@@ -3,6 +3,9 @@
    hash_fx : custom hash function
    """
 
+import csv
+from model.package import Package
+
 
 class HashTable:
 
@@ -32,3 +35,26 @@ class HashTable:
     def print_map(self):
         for i, val in enumerate(self.__map):
             print(i, ": ", val)
+
+
+def extract_csv(path, package_hash_table):
+    with open(path, 'r') as file:
+        csv_reader = csv.reader(file)
+
+        for column in csv_reader:
+            package_id = column[0]
+            address = column[1]
+            city = column[2]
+            state = column[3]
+            zip_code = column[4]
+            delivery_deadline = column[5]
+            mass = column[6]
+            package_status = column[7]
+            package = Package(package_id, address, city, state, zip_code, delivery_deadline, mass,
+                              package_status)
+
+            value = package
+
+            package_hash_table.insert_item(int(package_id), value)
+
+        return package_hash_table
--- main/main.py
+++ main/main.py
@@ -1,4 +1,6 @@
+import algo.hash
 import util
+from algo.graph import Graph
 from algo.hash import HashTable
 from util import extract_csv_hash
 import threading
@@ -6,20 +8,54 @@ import threading
 my_hash_table = HashTable()
 
 # parses .csv file and populates hash map
-file_path = "util/packageDestCSV.csv"
-extract_csv_hash.extract_csv(file_path, my_hash_table)
+package_file = "util/packageDestCSV.csv"
+algo.hash.extract_csv(package_file, my_hash_table)
 
-#my_hash_table.set_item(41,'test address')
+address_file = "util/addresses.csv"
+distance_table = "util/Distance_Table.csv"
 
-#my_hash_table.print_map()
+#
+# my_hash_table.insert_item(41, 'test address')
+#
+# my_hash_table.print_map()
+#
+# my_hash_table.lookup_item(21)
+#
+# print(my_hash_table.lookup_item(27))
+#
+# for i in range(1, 40):
+#     print(my_hash_table.lookup_item(i).address)
+#
+#
+# print(my_hash_table.lookup_item(27).address)
+
+my_graph = Graph()
+
+my_graph.add_vertex('A')
+my_graph.add_vertex('B')
+my_graph.add_vertex('C')
+my_graph.add_vertex('D')
+
+# my_graph.add_edge('A', 'B', 10)
+# my_graph.add_edge('A', 'C', 20)
+# my_graph.add_edge('A', 'D', 30)
+# my_graph.add_edge('B', 'D', 40)
+# my_graph.add_edge('C', 'D', 50)
+#
+# my_graph.delete_vertex('D')
+# my_graph.delete_vertex('D')
+#
+# my_graph.print_distance_graph()
+
+my_graph = Graph()
+
+my_graph.csv_parse_graph(address_file,distance_table)
+
+my_graph.populate_graph()
+
+my_graph.print_distance_graph()
+
+# ###############################loading graph###################################
 
 
-my_hash_table.lookup_item(21)
 
-print(my_hash_table.lookup_item(27))
-
-for i in range (1,40):
-    print(my_hash_table.lookup_item(i).address)
-#print(my_hash_table.get_item(41))
-
-print(my_hash_table.lookup_item(27).address)
